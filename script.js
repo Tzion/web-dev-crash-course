f76d4e71eae28d34e466ba0f6bfba60c3c32dea1
Tzion <tzion12345@gmail.com>
2021-09-13 17:50:12 +0300

    reuse the search button

--- script.js
+++ script.js
@@ -4,4 +4,8 @@
     btn.addEventListener( 'click', () => {
         overlay.style.display = 'grid'
         overlay.classList.add('animate-overlay')
+        setTimeout(() => {
+            overlay.style.display = 'none'
+        }, 3000);
+        document.documentElement.style.getPropertyValue('--overlay-delay')
     })
